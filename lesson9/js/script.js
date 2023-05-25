2b2f691f0eb28ae4823dab08a3e44382f0303a7a
taro sjk <tarosjk@gmail.com>
2023-05-25 14:47:02 +0900

    lesson9のメインビジュアルSPまで完了

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -1,2 +1,8 @@
 const navBtn = $('.header-button') //ボタン
 const gnav = $('.header-gnav') //ナビゲーション
+const body = $('body') //body要素
+
+// ボタンをクリックしたらbodyにクラスを追加・削除する
+navBtn.on('click', () => {
+  body.toggleClass('active')
+})
